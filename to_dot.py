d4c4e843a26a2e7f1b4409920d7392cc29afd66e
jkvis <jkvis@users.noreply.github.com>
2023-05-24 08:42:01 +0200

    Cleanup, plain text output and context list

--- to_dot.py
+++ to_dot.py
@@ -9,35 +9,30 @@ from api import get_version
 import config
 
 
-def read_relation(line):
-    lhs, rhs, predicate, *_ = line.split()
-    return lhs, rhs, predicate
-
-
-def read_relations(file):
+def read_relations(file=sys.stdin):
     relations = []
     for line in file.readlines():
-        lhs, rhs, predicate = read_relation(line)
-        relations.append((lhs, rhs, predicate))
+        lhs, rhs, value, *_ = line.split()
+        relations.append((lhs, rhs, value))
     return relations
 
 
-def write_edge(lhs, rhs, predicate):
-    if predicate == Relation.IS_CONTAINED.value:
+def dot_edge(lhs, rhs, value):
+    if value == Relation.IS_CONTAINED.value:
         return f'"{lhs}" -> "{rhs}";'
-    if predicate == Relation.CONTAINS.value:
+    if value == Relation.CONTAINS.value:
         return f'"{rhs}" -> "{lhs}";'
 
     rhs, lhs = sorted([lhs, rhs])
-    if predicate == Relation.EQUIVALENT.value:
+    if value == Relation.EQUIVALENT.value:
         return f'"{lhs}" -> "{rhs}" [arrowsize=0, color="black:invis:black"];'
-    if predicate == Relation.OVERLAP.value:
+    if value == Relation.OVERLAP.value:
         return f'"{lhs}" -> "{rhs}" [arrowsize=0, style=dashed];'
-    if predicate != Relation.DISJOINT.value:
-        raise ValueError(f"unknown relation: {predicate}")
+    if value != Relation.DISJOINT.value:
+        raise ValueError(f"unknown relation: {value}")
 
 
-def write_node(label, attributes):
+def dot_node(label, attributes):
     def quote(value):
         return f'"{value}"'
 
@@ -48,8 +43,8 @@ def write_dot(relations, nodes=None, file=sys.stdout):
     print("digraph {", file=file)
     if not nodes:
         nodes = {}
-    print("\n".join([write_node(key, value) for key, value in nodes.items()]), file=file)
-    print("\n".join([write_edge(*relation) for relation in relations]), file=file)
+    print("\n".join([dot_node(key, value) for key, value in nodes.items()]), file=file)
+    print("\n".join([dot_edge(*relation) for relation in relations]), file=file)
     print("}", file=file)
 
 
@@ -58,18 +53,17 @@ def build_graphs(relations):
     containment = nx.DiGraph()
     overlap = nx.Graph()
     for relation in relations:
-        lhs, rhs, predicate = relation
-        if predicate == Relation.EQUIVALENT.value:
+        lhs, rhs, value = relation
+        if value == Relation.EQUIVALENT.value:
             equivalent.add_edge(lhs, rhs)
-        elif predicate == Relation.IS_CONTAINED.value:
+        elif value == Relation.IS_CONTAINED.value:
             containment.add_edge(lhs, rhs)
-        elif predicate == Relation.CONTAINS.value:
+        elif value == Relation.CONTAINS.value:
             containment.add_edge(rhs, lhs)
-        elif predicate == Relation.OVERLAP.value:
+        elif value == Relation.OVERLAP.value:
             overlap.add_edge(lhs, rhs)
-        elif predicate != Relation.DISJOINT.value:
-            raise ValueError(f"unknown relation: {predicate}")
-
+        elif value != Relation.DISJOINT.value:
+            raise ValueError(f"unknown relation: {value}")
     return equivalent, containment, overlap
 
 
@@ -94,8 +88,7 @@ def overlap_without_common_ancestor(containment, overlap):
             ancestors[node] = set(nodes)
 
     selected = nx.Graph()
-    for edge in overlap.edges():
-        lhs, rhs = edge
+    for lhs, rhs in overlap.edges():
         if (lhs not in ancestors or rhs not in ancestors or
                 ancestors[lhs].isdisjoint(ancestors[rhs])):
             selected.add_edge(lhs, rhs)
@@ -105,8 +98,7 @@ def overlap_without_common_ancestor(containment, overlap):
 def most_specific_overlap(containment, overlap):
     to_remove = set()
     for node in overlap.nodes():
-        for pair in combinations(overlap.edges(node), 2):
-            (_, lhs), (_, rhs) = pair
+        for (_, lhs), (_, rhs) in combinations(overlap.edges(node), 2):
             if lhs in containment.nodes() and rhs in containment.nodes():
                 if lhs in nx.ancestors(containment, rhs):
                     to_remove.add((node, rhs))
@@ -118,20 +110,11 @@ def most_specific_overlap(containment, overlap):
     return overlap
 
 
-def export_relations(graph, predicate):
-    relations = []
-    for edge in graph.edges():
-        relations.append((*edge, predicate))
-    return relations
-
-
 def select_context(equivalent, containment, overlap, context):
     nodes = set(context)
     for node in context:
-        # TODO: no containment if there is already equivalence?
         if node in containment.nodes():
             nodes.update(list(nx.ancestors(containment, node)))
-        # TODO: no overlap if there is already containment or equivalence?
         if node in overlap.nodes():
             nodes.update(list(overlap.neighbors(node)))
 
@@ -156,7 +139,14 @@ def simplify(relations, context=None):
     return equivalent, containment, overlap
 
 
-def prepare4dot(equivalent, containment, overlap, nodes, context):
+def export_relations(graph, value):
+    relations = []
+    for edge in graph.edges():
+        relations.append((*edge, value))
+    return relations
+
+
+def prepare4export(equivalent, containment, overlap, nodes, context):
     for node in context:
         if node not in nodes:
             nodes[node] = {}
@@ -174,10 +164,11 @@ def prepare4dot(equivalent, containment, overlap, nodes, context):
 def main():
     parser = argparse.ArgumentParser(description="Create Graphviz dot file of relations")
     parser.add_argument("--gene", help="Gene to operate on", required=True)
-    parser.add_argument("--context", help="File with contextual nodes")
     parser.add_argument("--reference", help="Reference to operate on (default: %(default)s)", choices=["NG", "NC"], default="NG")
     parser.add_argument("--version", help="Specify PharmVar version")
     parser.add_argument("--disable-cache", help="Disable read and write from cache", action="store_true")
+    parser.add_argument("--context", nargs='*', help="List of contextual nodes")
+    parser.add_argument("--text", help="Plain text output", action="store_true")
 
     args = parser.parse_args()
 
@@ -187,7 +178,7 @@ def main():
     try:
         gene_info = config.get_gene(args.gene)
     except KeyError:
-        print(f"ERROR: Gene {args.gene} not in configuration!", file=sys.stderr)
+        print(f"ERROR: Gene {args.gene} not in configuration", file=sys.stderr)
         sys.exit(-1)
 
     if args.reference == "NG":
@@ -196,14 +187,13 @@ def main():
         ref_seq_id = gene_info["nc_ref_seq_id"]
 
     nodes = config.get_nodes(args.gene, args.version, not args.disable_cache, ref_seq_id)
+    relations, nodes = prepare4export(*simplify(read_relations(), args.context), nodes, args.context)
 
-    context = set()
-    if args.context:
-        with open(args.context, encoding="utf-8") as file:
-            context = set(file.read().splitlines())
-
-    relations = read_relations(sys.stdin)
-    write_dot(*prepare4dot(*simplify(relations, context), nodes, context))
+    if args.text:
+        for lhs, rhs, value in relations:
+            print(f"{lhs} {rhs} {value}")
+    else:
+        write_dot(relations, nodes)
 
 
 if __name__ == "__main__":
