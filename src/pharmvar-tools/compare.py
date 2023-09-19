117442356052cfc064170274161b1dacdf9aecb7
Mark Santcroos <m.a.santcroos@lumc.nl>
2023-09-19 23:05:20 +0200

    Convert to initworker.

--- src/pharmvar-tools/compare.py
+++ src/pharmvar-tools/compare.py
@@ -13,18 +13,19 @@ from check import allele_from_variants
 from config import get_gene
 
 
-# Global state for workers
-ctx_worker_alleles = {}
-ctx_worker_reference = ""
-
-
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 
+def init_worker(*data):
+    global worker_reference
+    global worker_alleles
+    worker_reference, worker_alleles = data
+
+
 def worker(args):
     lhs, rhs = args
-    relation = compare(ctx_worker_reference, ctx_worker_alleles[lhs], ctx_worker_alleles[rhs])
+    relation = compare(worker_reference, worker_alleles[lhs], worker_alleles[rhs])
     return lhs, rhs, relation
 
 
@@ -54,33 +55,30 @@ def main():
     with open(f"data/{ref_seq_id}.fasta", encoding="utf-8") as file:
         reference = fasta_sequence(file.readlines())
 
-    variants = get_variants(args.gene, ref_seq_id, args.version, not args.disable_cache)
-    alleles = get_alleles(args.gene, ref_seq_id, args.version, not args.disable_cache)
+    pv_variants = get_variants(args.gene, ref_seq_id, args.version, not args.disable_cache)
+    pv_alleles = get_alleles(args.gene, ref_seq_id, args.version, not args.disable_cache)
 
-    global ctx_worker_alleles
-    global ctx_worker_reference
-    ctx_worker_reference = reference
-
-    for allele in alleles:
+    alleles = {}
+    for allele in pv_alleles:
         try:
             allele_variants = allele_from_variants(reference, allele["variants"])
             observed = patch(reference, allele_variants)
             spanning = spanning_variant(reference, observed, allele_variants)
             supremal, *_ = find_supremal(reference, spanning)
-            ctx_worker_alleles[allele["name"]] = supremal
+            alleles[allele["name"]] = supremal
         except ValueError as e:
             eprint(f"ERROR: allele {allele['name']} - {e}")
 
-    for variant in variants:
+    for variant in pv_variants:
         try:
             allele = parse_hgvs(variant["hgvs"], reference)
             supremal, *_ = find_supremal(reference, allele[0])
-            ctx_worker_alleles[f"variant_{variant['id']}"] = supremal
+            alleles[f"variant_{variant['id']}"] = supremal
         except ValueError as e:
             eprint(f"ERROR: variant {variant['hgvs']} - {e}")
 
-    with Pool(args.cores) as pool:
-        relations = pool.map(worker, combinations(ctx_worker_alleles, 2))
+    with Pool(args.cores, initializer=init_worker, initargs=(reference, alleles)) as pool:
+        relations = pool.map(worker, combinations(alleles, 2))
         for lhs, rhs, relation in relations:
             if relation != Relation.DISJOINT:
                 print(lhs, rhs, relation.value)
